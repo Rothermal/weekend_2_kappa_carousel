47c3966a8ba23b29a1aa4f2511d437dd6e1c955c
Mark Rothermal <rothermal2@comcast.net>
2016-03-06 17:20:29 -0600

    added procfile

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ var express = require('express');
 var index = require('./route/index');
 var cohorts = require('./route/cohorts');
 var app = express();
-
+var herokuOrPort =  process.env.PORT || 1985;
 
 app.use(express.static('server/public'));
 
@@ -18,7 +18,7 @@ app.use('/',index);
 
 
 
-var server = app.listen(1985,function(){
+var server = app.listen(herokuOrPort,function(){
     var port = server.address().port;
     console.log('Serving Clients successfully since : ', port);
 });
